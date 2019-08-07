34f11bb6798837dc9df98ce6833382c5819cb61d
wulinyun <lin.wu@landasoft.com>
2019-08-07 12:21:15 +0800

    增加spring-boot-docker调用RestTemplate实体bean初始化

--- spring-boot-docker/src/main/java/com/landasoft/mas/demo/docker/SpringBootDockerApplication.java
+++ spring-boot-docker/src/main/java/com/landasoft/mas/demo/docker/SpringBootDockerApplication.java
@@ -2,13 +2,18 @@ package com.landasoft.mas.demo.docker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 /*@EnableWebSocket*/
 @SpringBootApplication
 public class SpringBootDockerApplication {
-
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerApplication.class, args);
 	}
