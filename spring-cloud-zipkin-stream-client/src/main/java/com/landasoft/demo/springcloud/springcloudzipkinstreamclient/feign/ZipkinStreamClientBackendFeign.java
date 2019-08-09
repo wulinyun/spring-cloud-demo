dc385af4347ba548e8643bd79ff31ae490f88fd7
wulinyun <lin.wu@landasoft.com>
2019-08-09 18:01:33 +0800

    增加feign调用接口代理

--- spring-cloud-zipkin-stream-client/src/main/java/com/landasoft/demo/springcloud/springcloudzipkinstreamclient/feign/ZipkinStreamClientBackendFeign.java
+++ spring-cloud-zipkin-stream-client/src/main/java/com/landasoft/demo/springcloud/springcloudzipkinstreamclient/feign/ZipkinStreamClientBackendFeign.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * feign调用客户端定义
+ * feign调用客户端定义  name的值为被调用服务中配置的spring.application.name的值
  * @author 伍林云
  * @date 2019/8/9  14:18
  */
