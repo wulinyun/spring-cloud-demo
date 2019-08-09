3767761915f855656ce802d96605fce2eaf6640f
wulinyun <lin.wu@landasoft.com>
2019-08-09 17:00:39 +0800

    增加swagger的配置

---
+++ spring-cloud-zipkin-stream-client-backend/src/main/java/com/landasoft/demo/spirngcloud/springcloudzipkinstreamclientbackend/config/Swagger2Config.java
@@ -0,0 +1,126 @@
+package com.landasoft.demo.spirngcloud.springcloudzipkinstreamclientbackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.OperationBuilderPlugin;
+import springfox.documentation.spi.service.contexts.OperationContext;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.*;
+
+/**
+ * swagger2 配置
+ * @author 伍林云
+ * @date 2019/8/9  16:45
+ */
+@EnableSwagger2
+@Configuration
+public class Swagger2Config {
+    @Value("${swagger.title}")
+    private String title;
+
+    @Value("${swagger.description}")
+    private String description;
+
+    @Value("${swagger.version}")
+    private String version;
+
+    @Value("${swagger.contact.name}")
+    private String name;
+
+    @Value("${swagger.contact.url}")
+    private String url;
+
+    @Value("${swagger.contact.email}")
+    private String email;
+
+    @Value("${swagger.show}")
+    private boolean swaggerShow;
+    @Bean
+    public Docket globalApi() {
+
+        List<Parameter> pars = new ArrayList<>();
+        //pars.add(new ParameterBuilder().name("corp_key").description("当前企业标识").modelRef(new ModelRef("string")).parameterType("header").required(false).build());
+        //pars.add(new ParameterBuilder().name("token").description("用户凭证").modelRef(new ModelRef("string")).parameterType("header").required(false).build());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .enable(swaggerShow)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars)
+                .securitySchemes(Collections.singletonList(new BasicAuth("basicAuth")))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo());
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(basicAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> basicAuth() {
+        return Collections.singletonList(SecurityReference.builder()
+                .reference("basicAuth")
+                .scopes(new AuthorizationScope[0])
+                .build());
+    }
+
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(title)
+                .description(description)
+                .contact(new Contact(name, url, email))
+                .version(version)
+                .build();
+    }
+
+    @Bean
+    public TranslationOperationBuilderPlugin translationPlugin() {
+        return new TranslationOperationBuilderPlugin();
+    }
+
+    @Order(Ordered.LOWEST_PRECEDENCE)
+    public static class TranslationOperationBuilderPlugin implements OperationBuilderPlugin {
+
+        @Autowired
+        private MessageSource translator;
+
+        @Override
+        public boolean supports(DocumentationType delimiter) {
+            return true;
+        }
+
+        @Override
+        public void apply(OperationContext context) {
+            Set<ResponseMessage> messages = context.operationBuilder().build().getResponseMessages();
+            Set<ResponseMessage> translated = new HashSet<>();
+            for (ResponseMessage untranslated : messages) {
+                String translation = translator.getMessage(untranslated.getMessage(), null, untranslated.getMessage(), null);
+                translated.add(new ResponseMessage(untranslated.getCode(),
+                        translation,
+                        untranslated.getResponseModel(),
+                        untranslated.getHeaders(),
+                        untranslated.getVendorExtensions()));
+            }
+            context.operationBuilder().responseMessages(translated);
+        }
+
+    }
+}
