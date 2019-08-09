8370f83c3cb1446b14291b0121e120c87ff2427f
wulinyun <lin.wu@landasoft.com>
2019-08-09 17:31:10 +0800

    加上swagger的接口描述

--- spring-cloud-zipkin-stream-client/src/main/java/com/landasoft/demo/springcloud/springcloudzipkinstreamclient/controller/UserController.java
+++ spring-cloud-zipkin-stream-client/src/main/java/com/landasoft/demo/springcloud/springcloudzipkinstreamclient/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.landasoft.demo.springcloud.springcloudzipkinstreamclient.controller;
 
 import com.landasoft.demo.springcloud.springcloudzipkinstreamclient.feign.ZipkinStreamClientBackendFeign;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +39,8 @@ public class UserController {
      * @param id
      * @return
      */
-    @RequestMapping("/call/{id}")
+    @ApiOperation(value = "直接调用接口地址")
+    @GetMapping("/call/{id}")
     public String callHome(@PathVariable String id){
         logger.info("calling from trace demo backend");
         String result= this.restTemplate.getForObject("http://localhost:11022/call/" + id, String.class);
@@ -49,7 +52,8 @@ public class UserController {
      * @param id
      * @return
      */
-    @RequestMapping("/call_f/{id}")
+    @ApiOperation(value = "通过feign调用接口地址")
+    @GetMapping("/call_f/{id}")
     public String callHomeF(@PathVariable String id){
         logger.info("calling from trace demo backend by feign");
         //String result= this.restTemplate.getForObject("http://localhost:11022/call/" + id, String.class);
@@ -62,19 +66,22 @@ public class UserController {
      * @param id
      * @return
      */
-    @RequestMapping("/call_e/{id}")
+    @ApiOperation(value = "通过注册中心的服务名称调用接口")
+    @GetMapping("/call_e/{id}")
     public String callHomeE(@PathVariable String id){
         logger.info("calling from trace demo backend by eureka");
         String result= this.restTemplate.getForObject("http://landa-mas-zipkin-stream-client-backend/call/" + id, String.class);
         return result+" world by eureka";
     }
-    @RequestMapping("/call1/{id}")
+    @ApiOperation(value = "直接接口地址调用中转调用另外一个服务")
+    @GetMapping("/call1/{id}")
     public String callHome1(@PathVariable String id){
         logger.info("calling from trace demo backend");
         String result= this.restTemplate.getForObject("http://localhost:11022/call1/" + id, String.class);
         return result+" world";
     }
-    @RequestMapping("/call2/{id}")
+    @ApiOperation(value = "直接调用另外一个服务")
+    @GetMapping("/call2/{id}")
     public String callSpringBootDocker(@PathVariable String id){
         logger.info("calling from trace demo backend spring-docker-demo");
         String result= this.restTemplate.getForObject("http://localhost:8080/rest/image/listImageDatas?userId=" + id, String.class);
