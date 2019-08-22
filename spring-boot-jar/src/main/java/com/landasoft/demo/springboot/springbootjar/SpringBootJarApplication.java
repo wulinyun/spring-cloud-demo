0ee31370542a7e97feb16e933779a5a640a0fcc0
wulinyun <lin.wu@landasoft.com>
2019-08-22 16:58:34 +0800

    spring-boot-jar的demo样例

---
+++ spring-boot-jar/src/main/java/com/landasoft/demo/springboot/springbootjar/SpringBootJarApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.springbootjar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJarApplication.class, args);
+    }
+
+}
