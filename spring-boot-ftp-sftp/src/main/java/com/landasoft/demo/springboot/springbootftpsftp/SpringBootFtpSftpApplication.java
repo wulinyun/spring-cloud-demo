bbba549b3f24511625995499d0fe81098db6e8d0
wulinyun <lin.wu@landasoft.com>
2019-08-23 16:03:36 +0800

    spring-boot-ftp-sftp的demo样例

---
+++ spring-boot-ftp-sftp/src/main/java/com/landasoft/demo/springboot/springbootftpsftp/SpringBootFtpSftpApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.springbootftpsftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFtpSftpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFtpSftpApplication.class, args);
+    }
+
+}
