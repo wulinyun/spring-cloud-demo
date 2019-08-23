a7cbdba2072805082e94083f6022a62fa11d9117
wulinyun <lin.wu@landasoft.com>
2019-08-23 21:36:50 +0800

    增加浏览器下载文件内容

--- spring-boot-ftp-sftp/src/main/java/com/landasoft/demo/springboot/springbootftpsftp/controller/FileController.java
+++ spring-boot-ftp-sftp/src/main/java/com/landasoft/demo/springboot/springbootftpsftp/controller/FileController.java
@@ -4,13 +4,19 @@ import com.landasoft.demo.springboot.springbootftpsftp.domain.Message;
 import com.landasoft.demo.springboot.springbootftpsftp.service.FileSystemService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -48,5 +54,35 @@ public class FileController {
             e.printStackTrace();
             return new Message(-1,"fail to upload "+filePath);
         }
+    }
+    @ApiOperation(value="浏览器下载文件")
+    public Message downLoad(@RequestParam(value = "targetPath")String targetPath ,HttpServletResponse response) throws Exception{
+        // 如果文件名不为空，则进行下载
+        InputStream inputStream = fileSystemService.getDownloadFileInputStream(targetPath);
+        String fileName = targetPath.substring(targetPath.lastIndexOf("/"));
+        if (inputStream != null) {
+            // 如果文件名存在，则进行下载
+                // 配置文件下载
+                response.setHeader("content-type", "application/octet-stream");
+                response.setContentType("application/octet-stream");
+                // 下载文件能正常显示中文
+                response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, "UTF-8"));
+
+                // 实现文件下载
+                byte[] buffer = new byte[1024];
+
+                try {
+                    OutputStream os = response.getOutputStream();
+                    IOUtils.copy(inputStream,os);
+                    System.out.println("Download the song successfully!");
+                }
+                catch (Exception e) {
+                    System.out.println("Download the song failed!");
+                }
+                finally {
+
+                }
+        }
+        return null;
     }
 }
