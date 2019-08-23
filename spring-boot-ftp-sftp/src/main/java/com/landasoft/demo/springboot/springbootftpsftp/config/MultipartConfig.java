6667b96618e3e11c1c8c9140dac0b4a2fb79a50e
wulinyun <lin.wu@landasoft.com>
2019-08-23 19:46:44 +0800

    增加MultipartConfig的上传限制配置

---
+++ spring-boot-ftp-sftp/src/main/java/com/landasoft/demo/springboot/springbootftpsftp/config/MultipartConfig.java
@@ -0,0 +1,30 @@
+package com.landasoft.demo.springboot.springbootftpsftp.config;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
+
+import javax.servlet.MultipartConfigElement;
+
+/**
+ * @author 伍林云
+ * @date 2019/8/23  19:43
+ */
+@Configuration
+public class MultipartConfig {
+    /**
+     * 文件上传配置
+     * @return
+     */
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //文件最大
+        factory.setMaxFileSize(DataSize.of(500,DataUnit.MEGABYTES));
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize(DataSize.of(500,DataUnit.MEGABYTES));
+        return factory.createMultipartConfig();
+    }
+}
