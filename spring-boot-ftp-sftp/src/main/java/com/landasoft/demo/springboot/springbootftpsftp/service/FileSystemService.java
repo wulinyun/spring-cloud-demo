14e518b8685d8a080d34c38bce72300a237fc309
wulinyun <lin.wu@landasoft.com>
2019-08-23 16:31:19 +0800

    spring-boot-ftp-sftp的demo样例

---
+++ spring-boot-ftp-sftp/src/main/java/com/landasoft/demo/springboot/springbootftpsftp/service/FileSystemService.java
@@ -0,0 +1,45 @@
+package com.landasoft.demo.springboot.springbootftpsftp.service;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * 文件系统服务接口定义
+ * @author 伍林云
+ * @date 2019/8/23  16:25
+ */
+public interface FileSystemService {
+    /**
+     * 通过源文件流上传到指定文件夹路径
+     * @param targetPath   目标路径
+     * @param inputStream   源文件流
+     * @return
+     * @throws Exception
+     */
+    boolean uploadFile(String targetPath, InputStream inputStream) throws Exception;
+
+    /**
+     * 通过源文件上传到指定路径
+     * @param targetPath  目标路径
+     * @param file  源文件
+     * @return
+     * @throws Exception
+     */
+    boolean uploadFile(String targetPath, File file) throws Exception;
+
+    /**
+     * 下载指定路径的文件
+     * @param targetPath    文件路径
+     * @return
+     * @throws Exception
+     */
+    File downloadFile(String targetPath) throws Exception;
+
+    /**
+     * 删除指定路径的文件
+     * @param targetPath    文件路径
+     * @return
+     * @throws Exception
+     */
+    boolean deleteFile(String targetPath) throws Exception;
+}
