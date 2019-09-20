12c0abcacd0b6fb183096e8935c37ebf227561ce
wulinyun <lin.wu@landasoft.com>
2019-09-20 15:33:38 +0800

    注释注册中心配置

--- spring-boot-jar/src/main/java/com/landasoft/demo/springboot/springbootjar/SpringBootJarApplication.java
+++ spring-boot-jar/src/main/java/com/landasoft/demo/springboot/springbootjar/SpringBootJarApplication.java
@@ -2,8 +2,10 @@ package com.landasoft.demo.springboot.springbootjar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+/*@EnableEurekaClient*/
 public class SpringBootJarApplication {
 
     public static void main(String[] args) {
