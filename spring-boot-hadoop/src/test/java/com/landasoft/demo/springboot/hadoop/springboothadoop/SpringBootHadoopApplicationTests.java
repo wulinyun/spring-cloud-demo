11927187df1b1aff8308384c656449ad1739017b
wulinyun <lin.wu@landasoft.com>
2019-12-03 14:28:09 +0800

    springboot操作hadoop的HA集群初始化代码

---
+++ spring-boot-hadoop/src/test/java/com/landasoft/demo/springboot/hadoop/springboothadoop/SpringBootHadoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.hadoop.springboothadoop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHadoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
