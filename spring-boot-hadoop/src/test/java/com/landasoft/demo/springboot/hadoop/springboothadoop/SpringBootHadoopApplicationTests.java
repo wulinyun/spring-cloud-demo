85e6b4c18d4fda17c6c37053e3476fd356d0fc27
wulinyun <lin.wu@landasoft.com>
2019-12-04 15:08:17 +0800

    创建文件夹删除文件夹单元测试

--- spring-boot-hadoop/src/test/java/com/landasoft/demo/springboot/hadoop/springboothadoop/SpringBootHadoopApplicationTests.java
+++ spring-boot-hadoop/src/test/java/com/landasoft/demo/springboot/hadoop/springboothadoop/SpringBootHadoopApplicationTests.java
@@ -1,13 +1,44 @@
 package com.landasoft.demo.springboot.hadoop.springboothadoop;
 
+import com.landasoft.demo.springboot.hadoop.springboothadoop.controller.HadoopController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootHadoopApplicationTests {
-
+    @Autowired
+    private HadoopController hadoopController;
     @Test
     void contextLoads() {
     }
+    @Test
+    public void deleteTest(){
+        String deletePath = "/hi";
+        try {
+            hadoopController.delete(deletePath,false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Test
+    public void mkdir() throws Exception {
+        String hello = hadoopController.mkdir("/dzqz/test");
+        System.out.println(hello);
+    }
+
+    @Test
+    public void uploadFile() throws Exception {
+        hadoopController.uploadFile("D:\\src\\upload.txt","/dzqz/test/test.txt",4096);
+    }
+    @Test
+    public void listFiles() throws Exception {
+        String path="/dzqz/";
+        hadoopController.getFileList(path);
+    }
+    @Test
+    public void deleteFile() throws Exception{
+        hadoopController.delete("/dzqz/test",true);
+    }
 }
