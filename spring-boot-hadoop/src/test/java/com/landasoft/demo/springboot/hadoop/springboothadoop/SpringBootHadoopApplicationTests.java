3846c42020e6c254107ddf8b0cfd76c09766e036
wulinyun <lin.wu@landasoft.com>
2019-12-04 15:13:46 +0800

    增加文件下载功能

--- spring-boot-hadoop/src/test/java/com/landasoft/demo/springboot/hadoop/springboothadoop/SpringBootHadoopApplicationTests.java
+++ spring-boot-hadoop/src/test/java/com/landasoft/demo/springboot/hadoop/springboothadoop/SpringBootHadoopApplicationTests.java
@@ -33,6 +33,10 @@ class SpringBootHadoopApplicationTests {
         hadoopController.uploadFile("D:\\src\\upload.txt","/dzqz/test/test.txt",4096);
     }
     @Test
+    public void downloadFile() throws Exception{
+        hadoopController.downloadFile("/dzqz/test.txt","D:\\src\\test.txt",128*1024);
+    }
+    @Test
     public void listFiles() throws Exception {
         String path="/dzqz/";
         hadoopController.getFileList(path);
