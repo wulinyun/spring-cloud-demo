efa07a8d412923f665b8ae553237076917a370e4
wulinyun <lin.wu@landasoft.com>
2019-12-09 18:58:21 +0800

    es的启动主类变更名字

--- spring-boot-elasticsearch7/src/main/java/com/landasoft/demo/springboot/elasticsearch7/springbootelasticsearch7/SpringBootElasticsearchApplication.java
+++ spring-boot-elasticsearch7/src/main/java/com/landasoft/demo/springboot/elasticsearch7/springbootelasticsearch7/SpringBootElasticsearchApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootElasticsearchApplication {
+public class SpringBootElasticsearch7Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootElasticsearchApplication.class, args);
+        SpringApplication.run(SpringBootElasticsearch7Application.class, args);
     }
 
 }
