73b38ef0f2096cf3c5277a4e285ee22a42cb4cb8
wulinyun <lin.wu@landasoft.com>
2019-12-09 16:56:04 +0800

    修改包名为elasticsearch7

--- spring-boot-elasticsearch7/src/main/java/com/landasoft/demo/springboot/elasticsearch7/springbootelasticsearch7/SpringBootElasticsearchApplication.java
+++ spring-boot-elasticsearch7/src/main/java/com/landasoft/demo/springboot/elasticsearch7/springbootelasticsearch7/SpringBootElasticsearchApplication.java
@@ -1,4 +1,4 @@
-package com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch;
+package com.landasoft.demo.springboot.elasticsearch7.springbootelasticsearch7;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
