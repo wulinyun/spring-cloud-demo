2797142376a47db89f580b9e6c9242d377567403
wulinyun <lin.wu@landasoft.com>
2019-12-09 19:02:24 +0800

    修改单元测试主类名称

---
+++ spring-boot-elasticsearch7/src/test/java/com/landasoft/demo/springboot/elasticsearch7/springbootelasticsearch7/SpringBootElasticsearch7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.elasticsearch7.springbootelasticsearch7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootElasticsearch7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
