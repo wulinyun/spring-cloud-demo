c8436f86773c21c3d0983478d9a57d95e84c540a
wulinyun <lin.wu@landasoft.com>
2019-12-16 18:33:43 +0800

    开启feignclient、异步、属性配置功能

--- spring-boot-elasticsearch/src/main/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/SpringBootElasticsearchApplication.java
+++ spring-boot-elasticsearch/src/main/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/SpringBootElasticsearchApplication.java
@@ -2,8 +2,14 @@ package com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableAsync
+@EnableConfigurationProperties
 public class SpringBootElasticsearchApplication {
 
     public static void main(String[] args) {
