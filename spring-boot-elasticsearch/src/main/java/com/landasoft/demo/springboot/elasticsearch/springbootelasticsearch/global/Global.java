17f0c95f57c6b389a8720a2870aa3b687047387e
wulinyun <lin.wu@landasoft.com>
2019-12-17 17:27:58 +0800

    全局常量

---
+++ spring-boot-elasticsearch/src/main/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/global/Global.java
@@ -0,0 +1,22 @@
+package com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.global;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description 全局常量
+ * @Date 2019/12/17 16:52
+ */
+public interface Global {
+    String INDEX_NAME = "indexName";
+    String INDEX_TYPE = "indexType";
+    String PAGE = "page";
+    String SIZE = "size";
+    String Q = "q";
+    String MISSING = "m";
+    String EXISTS = "e";
+    String FIELDS = "fields";
+    String SORT = "sort";
+    String SPLIT_FLAG_COMMA = ",";
+    String NON_FLAG = "_";
+    String WILDCARD = "*";
+}
