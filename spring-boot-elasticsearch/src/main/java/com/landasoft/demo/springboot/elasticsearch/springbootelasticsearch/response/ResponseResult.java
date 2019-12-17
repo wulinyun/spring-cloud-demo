fb86615f1d4c9f8e643786be033313d08c03050b
wulinyun <lin.wu@landasoft.com>
2019-12-17 17:30:57 +0800

    后台响应结果类

---
+++ spring-boot-elasticsearch/src/main/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/response/ResponseResult.java
@@ -0,0 +1,70 @@
+package com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.exception.ApiException;
+import com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.response.enums.CodeEnum;
+import lombok.Data;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.ALWAYS;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description
+ * @Date 2019/12/17 16:29
+ */
+@Data
+@JsonInclude(ALWAYS)
+public class ResponseResult<T> {
+    private String code;
+
+    private String message;
+
+    private T data;
+
+
+    public ResponseResult(ApiException e) {
+        this.code = e.getCode();
+        this.message = e.getMessage();
+    }
+
+    public ResponseResult(T data) {
+        this.code = CodeEnum.SUCCESS.getCode();
+        this.message = CodeEnum.SUCCESS.getMessage();
+        this.data = data;
+    }
+
+    public ResponseResult(String errCode, String errMsg, T data) {
+        this.code = errCode;
+        this.message = errMsg;
+        this.data = data;
+    }
+
+    public ResponseResult() {}
+
+    public static <T> ResponseResult<T> success(T data) {
+        return new ResponseResult<>(data);
+    }
+
+    public static <T>ResponseResult<T> badRequestError(String message) {
+        return new ResponseResult<>(CodeEnum.BAD_REQUEST_CODE.getCode(), message, null);
+    }
+
+
+    /**
+     * 返回异常
+     *
+     * @param e Exception
+     * @return ResponseResult
+     */
+    public static <T> ResponseResult<T> fail(Throwable e) {
+        if (e instanceof ApiException) {
+            return new ResponseResult<>((ApiException) e);
+        }
+        return new ResponseResult<>(CodeEnum.SYSTEM_ERROR_CODE.getCode(), e.getMessage(), null);
+    }
+
+    public static ResponseResult fail(ApiException e, Object data) {
+        return new ResponseResult<>(e.getCode(), e.getMessage(), data);
+    }
+}
