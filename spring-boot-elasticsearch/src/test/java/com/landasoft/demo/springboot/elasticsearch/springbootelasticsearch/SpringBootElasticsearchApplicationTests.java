8779424ec4785c8dd655230af99e7fb7f8b582cb
wulinyun <lin.wu@landasoft.com>
2019-12-19 16:28:20 +0800

    修改保存文档

--- spring-boot-elasticsearch/src/test/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
+++ spring-boot-elasticsearch/src/test/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
 class SpringBootElasticsearchApplicationTests {
     @Resource
     private ElasticsearchTemplate elasticsearchTemplate;
+    @Resource
     private StoreRepository storeRepository;
     @Test
     void contextLoads() {
