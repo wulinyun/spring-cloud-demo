13e1bea70777b810f3a3dc77b4a6e9ef00e6486e
wulinyun <lin.wu@landasoft.com>
2019-12-19 13:51:02 +0800

    swagger2配置类

---
+++ spring-boot-elasticsearch/src/main/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/config/Swagger2Config.java
@@ -0,0 +1,48 @@
+package com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description
+ * @Date 2019/12/10 18:01
+ */
+@Configuration
+@EnableSwagger2
+@ConditionalOnProperty(prefix = "swagger",value = {"enable"},havingValue = "true")
+public class Swagger2Config implements WebMvcConfigurer {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))                         //这里采用包含注解的方式来确定要显示的接口
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API")
+                .description("SpringBoot整合es案例 Api文档")
+                .termsOfServiceUrl("暂无")
+                .version("2.0")
+                .build();
+    }
+
+
+
+}
