c1d9a951f79c04d7fa6042ec6332e7faf9c43553
wulinyun <lin.wu@landasoft.com>
2019-12-19 16:23:21 +0800

    增加单元测试案例

--- spring-boot-elasticsearch/src/test/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
+++ spring-boot-elasticsearch/src/test/java/com/landasoft/demo/springboot/elasticsearch/springbootelasticsearch/SpringBootElasticsearchApplicationTests.java
@@ -1,13 +1,46 @@
 package com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch;
 
+import com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.docindex.repo.StoreRepository;
+import com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.document.StoreDocument;
+import com.landasoft.demo.springboot.elasticsearch.springbootelasticsearch.document.store.StoreBaseInfo;
+import org.joda.time.DateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
+
+import javax.annotation.Resource;
 
 @SpringBootTest
 class SpringBootElasticsearchApplicationTests {
-
+    @Resource
+    private ElasticsearchTemplate elasticsearchTemplate;
+    private StoreRepository storeRepository;
     @Test
     void contextLoads() {
     }
 
+    /**
+     * 创建索引，创建映射
+     */
+    @Test
+    public void testCreate(){
+        //创建索引
+        System.out.println(elasticsearchTemplate.createIndex(StoreDocument.class));
+        //创建映射
+        System.out.println(elasticsearchTemplate.putMapping(StoreDocument.class));
+    }
+
+    /**
+     * 新增修改文档
+     */
+    @Test
+    public void testSave(){
+        StoreDocument storeDocument = new StoreDocument();
+        storeDocument.setId("1");
+        StoreBaseInfo baseInfo = new StoreBaseInfo();
+        baseInfo.setStoreId("1");
+        baseInfo.setCreatedTime(DateTime.now());
+        storeDocument.setBaseInfo(baseInfo);
+        storeRepository.save(storeDocument);
+    }
 }
