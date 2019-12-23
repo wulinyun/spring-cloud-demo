58355c414ae9e97c589e313ad3d1efed6b49012e
wulinyun <lin.wu@landasoft.com>
2019-12-23 19:04:10 +0800

    spring-boot-rabbitmq的生产者初始化代码

---
+++ spring-boot-rabbitmq-sender/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/SpringBootRabbitmqSenderApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqSenderApplication.class, args);
+    }
+
+}
---
+++ spring-boot-rabbitmq-sender/src/test/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/SpringBootRabbitmqSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
