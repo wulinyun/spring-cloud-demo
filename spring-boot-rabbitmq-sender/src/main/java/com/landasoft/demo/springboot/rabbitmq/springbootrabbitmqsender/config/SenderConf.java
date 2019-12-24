bec1e843bc65975831a54053fa2827984aca3807
wulinyun <lin.wu@landasoft.com>
2019-12-24 09:32:52 +0800

    direct路由模式应用场景描述添加

--- spring-boot-rabbitmq-sender/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/config/SenderConf.java
+++ spring-boot-rabbitmq-sender/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/config/SenderConf.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
  * @Version 1.0
  * @Description direct模式相当于一对一模式,一个消息被发送者发送后,会被转发到一个绑定的消息队列中,然后被一个接收者接收!
  * 实际上RabbitMQ还可以支持发送对象:当然由于涉及到序列化和反序列化,该对象要实现Serilizable接口
+ * 应用场景：单发送，单接收的简单的应用场景
  * @Date 2019/12/24 9:01
  */
 @Configuration
