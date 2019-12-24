b4f6fe447e6731ece9b3e5511be1fb2176cdeb30
wulinyun <lin.wu@landasoft.com>
2019-12-24 10:06:46 +0800

    topic通配符主题订阅模式

--- spring-boot-rabbitmq-sender/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/config/SenderTopicConf.java
+++ spring-boot-rabbitmq-sender/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/config/SenderTopicConf.java
@@ -1,5 +1,11 @@
 package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -14,4 +20,25 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class SenderTopicConf {
+    @Bean(name="message")
+    public Queue queueMessage() {
+        return new Queue("topic.message",true);
+    }
+    @Bean(name="messages")
+    public Queue queueMessages() {
+        return new Queue("topic.messages",true);
+    }
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange("exchange", true, false);
+    }
+    @Bean
+    Binding bindingExchangeMessage(@Qualifier("message") Queue queueMessage,
+                                   TopicExchange exchange) {
+        return BindingBuilder.bind(queueMessage).to(exchange).with("topic.message");
+    }
+    @Bean
+    Binding bindingExchangeMessages(@Qualifier("messages") Queue queueMessages, TopicExchange exchange) {
+        return BindingBuilder.bind(queueMessages).to(exchange).with("topic.#");//*表示一个词,#表示零个或多个词
+    }
 }
