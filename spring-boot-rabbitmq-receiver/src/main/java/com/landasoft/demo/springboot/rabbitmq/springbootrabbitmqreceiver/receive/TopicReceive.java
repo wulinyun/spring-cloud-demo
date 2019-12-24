f4c604510d4e28ef48b46dc8da3b69798f4e5643
wulinyun <lin.wu@landasoft.com>
2019-12-24 11:59:41 +0800

    Topic增加自定义消息的监听接收

--- spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/TopicReceive.java
+++ spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/TopicReceive.java
@@ -1,5 +1,6 @@
 package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqreceiver.receive;
 
+import com.landasoft.demo.springboot.rabbitmq.bean.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -15,10 +16,18 @@ import org.springframework.stereotype.Component;
 public class TopicReceive {
     @RabbitListener(queues="topic.message")    //监听器监听指定的Queue
     public void processC1(String str) {
-        log.info("Receive:"+str);
+        log.info("ReceiveC1:"+str);
+    }
+    @RabbitListener(queues="topic.message")    //监听器监听指定的Queue
+    public void processC1(User user) {
+        log.info("ReceiveC1:"+user);
     }
     @RabbitListener(queues="topic.messages")    //监听器监听指定的Queue
     public void processC2(String str) {
-        log.info("Receive:"+str);
+        log.info("ReceiveC2:"+str);
+    }
+    @RabbitListener(queues="topic.messages")    //监听器监听指定的Queue
+    public void processC2(User user) {
+        log.info("ReceiveC2:"+user);
     }
 }
