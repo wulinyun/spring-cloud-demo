4611a060dfe83f9f9ecb7974ad19910196d287a2
wulinyun <lin.wu@landasoft.com>
2019-12-24 12:21:23 +0800

    广播监听的时候最好不要出现多类型接收参数，因为广播每个监听都会受到会出现转发异常的错误

--- spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/FanoutReceive.java
+++ spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/FanoutReceive.java
@@ -14,26 +14,26 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class FanoutReceive {
-    @RabbitListener(queues="fanout.A")    //监听器监听指定的Queue
+    /*@RabbitListener(queues="fanout.A")    //监听器监听指定的Queue
     public void processA(String str) {
         log.info("ReceiveA:"+str);
-    }
+    }*/
     @RabbitListener(queues="fanout.A")    //监听器监听指定的Queue
     public void processA(User user) {
         log.info("ReceiveA:"+user);
     }
-    @RabbitListener(queues="fanout.B")    //监听器监听指定的Queue
+    /*@RabbitListener(queues="fanout.B")    //监听器监听指定的Queue
     public void processB(String str) {
         log.info("ReceiveB:"+str);
-    }
+    }*/
     @RabbitListener(queues="fanout.B")    //监听器监听指定的Queue
     public void processB(User user) {
         log.info("ReceiveB:"+user);
     }
-    @RabbitListener(queues="fanout.C")    //监听器监听指定的Queue
+   /* @RabbitListener(queues="fanout.C")    //监听器监听指定的Queue
     public void processC(String str) {
         log.info("ReceiveC:"+str);
-    }
+    }*/
     @RabbitListener(queues="fanout.C")    //监听器监听指定的Queue
     public void processC(User user) {
         log.info("ReceiveC:"+user);
