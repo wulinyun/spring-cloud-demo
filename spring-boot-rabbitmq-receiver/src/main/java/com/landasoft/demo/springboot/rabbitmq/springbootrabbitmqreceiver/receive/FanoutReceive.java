1ae5427da9a4c221efc086dfeb9cb72e848c7dc0
wulinyun <lin.wu@landasoft.com>
2019-12-24 11:57:29 +0800

    增加监听接收自定义序列化对象的广播监听器

--- spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/FanoutReceive.java
+++ spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/FanoutReceive.java
@@ -1,5 +1,6 @@
 package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqreceiver.receive;
 
+import com.landasoft.demo.springboot.rabbitmq.bean.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -17,12 +18,24 @@ public class FanoutReceive {
     public void processA(String str) {
         log.info("ReceiveA:"+str);
     }
+    @RabbitListener(queues="fanout.A")    //监听器监听指定的Queue
+    public void processA(User user) {
+        log.info("ReceiveA:"+user);
+    }
     @RabbitListener(queues="fanout.B")    //监听器监听指定的Queue
     public void processB(String str) {
         log.info("ReceiveB:"+str);
     }
+    @RabbitListener(queues="fanout.B")    //监听器监听指定的Queue
+    public void processB(User user) {
+        log.info("ReceiveB:"+user);
+    }
     @RabbitListener(queues="fanout.C")    //监听器监听指定的Queue
     public void processC(String str) {
         log.info("ReceiveC:"+str);
+    }
+    @RabbitListener(queues="fanout.C")    //监听器监听指定的Queue
+    public void processC(User user) {
+        log.info("ReceiveC:"+user);
     }
 }
