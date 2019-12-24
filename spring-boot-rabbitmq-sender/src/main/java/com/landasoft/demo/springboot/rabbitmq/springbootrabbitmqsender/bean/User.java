395d076ff29f91bf881100f5dc9d815d8dc9d94e
wulinyun <lin.wu@landasoft.com>
2019-12-24 10:15:45 +0800

    发送消息体

---
+++ spring-boot-rabbitmq-sender/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/bean/User.java
@@ -0,0 +1,23 @@
+package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.bean;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description 发送消息体
+ * @Date 2019/12/24 10:11
+ */
+@Data
+public class User implements Serializable {
+    /**
+     * 用户名
+     */
+    private String userName;
+    /**
+     * 用户密码
+     */
+    private String userPassword;
+}
