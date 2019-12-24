52a2f10c92e783530cbbc865dd1e55d707754bfd
wulinyun <lin.wu@landasoft.com>
2019-12-24 11:11:43 +0800

    spring-boot-rabbitmq-receiver项目初始化代码

---
+++ spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/SpringBootRabbitmqReceiverApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqReceiverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqReceiverApplication.class, args);
+    }
+
+}
---
+++ spring-boot-rabbitmq-receiver/src/test/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/SpringBootRabbitmqReceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqReceiverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
