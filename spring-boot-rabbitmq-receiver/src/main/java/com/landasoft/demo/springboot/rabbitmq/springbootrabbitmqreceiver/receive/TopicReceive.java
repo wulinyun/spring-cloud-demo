4ab4fd37eaa2e853c374fb7193131476a21413e6
wulinyun <lin.wu@landasoft.com>
2019-12-24 15:10:43 +0800

    修改Topic的监听器输出提示

--- spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/TopicReceive.java
+++ spring-boot-rabbitmq-receiver/src/main/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqreceiver/receive/TopicReceive.java
@@ -16,18 +16,18 @@ import org.springframework.stereotype.Component;
 public class TopicReceive {
     /*@RabbitListener(queues="topic.message")    //监听器监听指定的Queue
     public void processC1(String str) {
-        log.info("ReceiveC1:"+str);
+        log.info("topic.message Receive:"+str);
     }*/
     @RabbitListener(queues="topic.message")    //监听器监听指定的Queue
     public void processC1(User user) {
-        log.info("ReceiveC1:"+user);
+        log.info("topic.message Receive :"+user);
     }
     /*@RabbitListener(queues="topic.messages")    //监听器监听指定的Queue
     public void processC2(String str) {
-        log.info("ReceiveC2:"+str);
+        log.info("topic.messages Receive:"+str);
     }*/
     @RabbitListener(queues="topic.messages")    //监听器监听指定的Queue
     public void processC2(User user) {
-        log.info("ReceiveC2:"+user);
+        log.info("topic.messages Receive:"+user);
     }
 }
