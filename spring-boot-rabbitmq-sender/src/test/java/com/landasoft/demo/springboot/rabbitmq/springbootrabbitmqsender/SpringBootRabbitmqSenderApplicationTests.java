4ccea6619e7f36df689a4c3721b6baecb71cbb79
wulinyun <lin.wu@landasoft.com>
2019-12-24 10:51:52 +0800

    rabbitmq关于direct路由模式、topic通配符订阅模式、fanout广播模式消息发送单元测试

--- spring-boot-rabbitmq-sender/src/test/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/SpringBootRabbitmqSenderApplicationTests.java
+++ spring-boot-rabbitmq-sender/src/test/java/com/landasoft/demo/springboot/rabbitmq/springbootrabbitmqsender/SpringBootRabbitmqSenderApplicationTests.java
@@ -1,13 +1,50 @@
 package com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender;
 
+import com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.bean.User;
+import com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.config.SenderFanoutConf;
+import com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.config.SenderTopicConf;
+import com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.sender.FanoutSender;
+import com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.sender.Sender;
+import com.landasoft.demo.springboot.rabbitmq.springbootrabbitmqsender.sender.TopicSender;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootRabbitmqSenderApplicationTests {
+    @Autowired
+    private Sender sender;//direct路由模式消息发送器
+    @Autowired
+    private TopicSender topicSender;//topic通配符订阅模式消息发送器
+    @Autowired
+    private FanoutSender fanoutSender;//fanout广播模式消息发送器
 
     @Test
     void contextLoads() {
+    }
+    @Test
+    public void testRabbit() {
+        sender.send("hello,rabbit~");
+        User user = new User();
+        user.setUserName("wulinyun");
+        user.setUserPassword("88888888");
+        sender.send(user);
+    }
+    @Test
+    public void testTopicRabbit() {
+        topicSender.send(SenderTopicConf.EXCHANGE, "topic.message", "hello,rabbit~");;
+        User user = new User();
+        user.setUserName("wulinyun");
+        user.setUserPassword("88888888");
+        //topicSender.send(SenderTopicConf.EXCHANGE, "topic.messages", user);
+    }
+    @Test
+    public void testFanoutRabbit() {
+        fanoutSender.send(SenderFanoutConf.FANOUTEXCHANGE, "", "hello,fanout rabbit~");;
+        User user = new User();
+        user.setUserName("wulinyun");
+        user.setUserPassword("88888888");
+        //fanoutSender.send("fanoutExchange", "", user);
     }
 
 }
