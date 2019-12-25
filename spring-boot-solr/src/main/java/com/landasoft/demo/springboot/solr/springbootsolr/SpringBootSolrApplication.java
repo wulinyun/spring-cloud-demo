1f18c60cbe175eada436acffa2173f5e850268b5
wulinyun <lin.wu@landasoft.com>
2019-12-25 14:57:23 +0800

    spring-boot-solr的初始化项目代码

---
+++ spring-boot-solr/src/main/java/com/landasoft/demo/springboot/solr/springbootsolr/SpringBootSolrApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.solr.springbootsolr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSolrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSolrApplication.class, args);
+    }
+
+}
---
+++ spring-boot-solr/src/test/java/com/landasoft/demo/springboot/solr/springbootsolr/SpringBootSolrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springboot.solr.springbootsolr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSolrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
