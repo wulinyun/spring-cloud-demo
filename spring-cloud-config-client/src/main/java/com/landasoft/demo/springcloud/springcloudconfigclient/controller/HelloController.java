07f76ee1623290ed73d417cb22b3c9b194ae9328
wulinyun <lin.wu@landasoft.com>
2019-12-30 17:27:47 +0800

    spring-cloud-config-client控制器测试方法

--- spring-cloud-config-client/src/main/java/com/landasoft/demo/springcloud/springcloudconfigclient/controller/HelloController.java
+++ spring-cloud-config-client/src/main/java/com/landasoft/demo/springcloud/springcloudconfigclient/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.landasoft.demo.springcloud.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
  * @Author wulinyun
  * @Version 1.0
  * @Description 测试spring-cloud-config-client测试方法
+ * 注明：自动刷新只能刷新 @RefreshScope 注解下的配置，一些特殊配置，如数据库等，需要同样先设置数据库链接ConfigServer类，然后通过加 @RefreshScope 注解方式
  * @Date 2019/12/30 13:53
  */
 @RestController
+@RefreshScope
 public class HelloController {
     @Value("${easy.hello}")
     private String msg;
