808590a31008c61793fc2529d1c4719cbd9e3ae8
wulinyun <lin.wu@landasoft.com>
2019-12-30 18:06:22 +0800

    增加注册中心注解

--- spring-cloud-config-client/src/main/java/com/landasoft/demo/springcloud/springcloudconfigclient/SpringCloudConfigClientApplication.java
+++ spring-cloud-config-client/src/main/java/com/landasoft/demo/springcloud/springcloudconfigclient/SpringCloudConfigClientApplication.java
@@ -2,8 +2,10 @@ package com.landasoft.demo.springcloud.springcloudconfigclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringCloudConfigClientApplication {
 
     public static void main(String[] args) {
