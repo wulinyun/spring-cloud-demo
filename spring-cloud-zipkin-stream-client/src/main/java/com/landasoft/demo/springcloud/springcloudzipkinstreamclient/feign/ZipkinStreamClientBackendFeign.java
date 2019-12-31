bc3e4776ec1483fd9ebd81e8dcbb49fb326cc3a6
wulinyun <lin.wu@landasoft.com>
2019-12-31 17:32:53 +0800

    测试feignClient调用超时问题，只需要配置 feign:   client:     config:       landa-mas-zipkin-stream-client-backend:         connectTimeout: 3600000         readTimeout: 3600000

--- spring-cloud-zipkin-stream-client/src/main/java/com/landasoft/demo/springcloud/springcloudzipkinstreamclient/feign/ZipkinStreamClientBackendFeign.java
+++ spring-cloud-zipkin-stream-client/src/main/java/com/landasoft/demo/springcloud/springcloudzipkinstreamclient/feign/ZipkinStreamClientBackendFeign.java
@@ -3,6 +3,7 @@ package com.landasoft.demo.springcloud.springcloudzipkinstreamclient.feign;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * feign调用客户端定义  name的值为被调用服务中配置的spring.application.name的值
@@ -12,5 +13,5 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(name = "landa-mas-zipkin-stream-client-backend")
 public interface ZipkinStreamClientBackendFeign {
     @RequestMapping("/call/{id}")
-    String call(@PathVariable("id") String id);
+    String call(@PathVariable("id") String id, @RequestParam(name = "time",required = false)Long time);
 }
