cd209d88220217a2d9b58cf2f3743ff1f3c421eb
wulinyun <lin.wu@landasoft.com>
2019-12-31 10:18:04 +0800

    增加自动刷新的RefreshScope注解

--- spring-cloud-config-client/src/main/java/com/landasoft/demo/springcloud/springcloudconfigclient/controller/HelloController.java
+++ spring-cloud-config-client/src/main/java/com/landasoft/demo/springcloud/springcloudconfigclient/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @Author wulinyun
  * @Version 1.0
- * @Description 测试spring-cloud-config-client测试方法
+ * @Description 测试spring-cloud-configs-client测试方法
  * 注明：自动刷新只能刷新 @RefreshScope 注解下的配置，一些特殊配置，如数据库等，需要同样先设置数据库链接ConfigServer类，然后通过加 @RefreshScope 注解方式
  * @Date 2019/12/30 13:53
  */
