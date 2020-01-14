21ce93d64633b8d61dd1e70a84e6433649f41443
wulinyun <lin.wu@landasoft.com>
2020-01-14 16:30:08 +0800

    增加单个服务的hystrix的监控实体bean以及配置

--- spring-cloud-hystrix-dashboard/src/main/java/com/landasoft/demo/springcloud/springcloudhystrixdashboard/SpringCloudHystrixDashboardApplication.java
+++ spring-cloud-hystrix-dashboard/src/main/java/com/landasoft/demo/springcloud/springcloudhystrixdashboard/SpringCloudHystrixDashboardApplication.java
@@ -1,9 +1,12 @@
 package com.landasoft.demo.springcloud.springcloudhystrixdashboard;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
 
 @EnableHystrix
 @EnableHystrixDashboard
@@ -13,5 +16,13 @@ public class SpringCloudHystrixDashboardApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudHystrixDashboardApplication.class, args);
     }
-
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
