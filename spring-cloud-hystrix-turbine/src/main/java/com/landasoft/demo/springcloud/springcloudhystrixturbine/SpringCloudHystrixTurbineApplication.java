4885c421a9e6017aa853e6e2ac55d391a61019c8
wulinyun <lin.wu@landasoft.com>
2020-01-14 16:50:22 +0800

    spring-cloud的初始化项目

---
+++ spring-cloud-hystrix-turbine/src/main/java/com/landasoft/demo/springcloud/springcloudhystrixturbine/SpringCloudHystrixTurbineApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudhystrixturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHystrixTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixTurbineApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-hystrix-turbine/src/test/java/com/landasoft/demo/springcloud/springcloudhystrixturbine/SpringCloudHystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudhystrixturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudHystrixTurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
