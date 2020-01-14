117e0f0f2fdbdce548ef19e3616f4fb91c230206
wulinyun <lin.wu@landasoft.com>
2020-01-14 18:08:11 +0800

    开启断路器注解

--- spring-cloud-hystrix/src/main/java/com/landasoft/demo/springcloud/springcloudhystrix/SpringCloudHystrixApplication.java
+++ spring-cloud-hystrix/src/main/java/com/landasoft/demo/springcloud/springcloudhystrix/SpringCloudHystrixApplication.java
@@ -1,13 +1,27 @@
 package com.landasoft.demo.springcloud.springcloudhystrix;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+@EnableHystrix
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudHystrixApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudHystrixApplication.class, args);
     }
-
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
 }
