863aa715b8c948cbeeff4ded5e8ca2725912bb6c
wulinyun <lin.wu@landasoft.com>
2020-01-14 09:58:30 +0800

    spring-cloud-hystrix-dashboard

---
+++ spring-cloud-hystrix-dashboard/src/main/java/com/landasoft/demo/springcloud/springcloudhystrixdashboard/SpringCloudHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudHystrixDashboardApplication.class, args);
+    }
+
+}
