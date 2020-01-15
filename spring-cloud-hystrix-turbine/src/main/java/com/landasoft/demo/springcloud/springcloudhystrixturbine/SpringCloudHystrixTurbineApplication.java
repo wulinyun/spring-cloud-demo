c6c5cb91afbd856e8beea977cd64c7af616fdfb0
wulinyun <lin.wu@landasoft.com>
2020-01-15 09:38:55 +0800

    配置调整

--- spring-cloud-hystrix-turbine/src/main/java/com/landasoft/demo/springcloud/springcloudhystrixturbine/SpringCloudHystrixTurbineApplication.java
+++ spring-cloud-hystrix-turbine/src/main/java/com/landasoft/demo/springcloud/springcloudhystrixturbine/SpringCloudHystrixTurbineApplication.java
@@ -2,12 +2,17 @@ package com.landasoft.demo.springcloud.springcloudhystrixturbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
+@EnableHystrixDashboard
+@EnableTurbine
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SpringCloudHystrixTurbineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudHystrixTurbineApplication.class, args);
     }
-
 }
