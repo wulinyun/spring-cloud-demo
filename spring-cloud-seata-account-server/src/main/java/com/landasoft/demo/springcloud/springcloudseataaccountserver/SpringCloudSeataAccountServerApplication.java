a8ee6e86fc7998d0b6e768e2d2a633d9c0a331df
wulinyun <lin.wu@landasoft.com>
2020-01-17 15:21:54 +0800

    开启服务发现、feignClient、禁用数据源自动注入

--- spring-cloud-seata-account-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/SpringCloudSeataAccountServerApplication.java
+++ spring-cloud-seata-account-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/SpringCloudSeataAccountServerApplication.java
@@ -1,9 +1,16 @@
 package com.landasoft.demo.springcloud.springcloudseataaccountserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.landasoft.demo.springcloud.springcloudseataaccountserver.dao")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SpringCloudSeataAccountServerApplication {
 
     public static void main(String[] args) {
