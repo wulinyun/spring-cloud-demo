eb743d9d48d59a5241af2decdba28f293ce78d08
wulinyun <lin.wu@landasoft.com>
2020-01-17 14:10:06 +0800

    account-server初始化项目代码

---
+++ spring-cloud-seata-account-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/SpringCloudSeataAccountServerApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudseataaccountserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSeataAccountServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSeataAccountServerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-seata-account-server/src/test/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/SpringCloudSeataAccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudseataaccountserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSeataAccountServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
