74dfaf0002c8833aa28c166d8bc1ce5416d82503
wulinyun <lin.wu@landasoft.com>
2020-01-17 15:43:07 +0800

    订单分布式事务初始化项目代码

---
+++ spring-cloud-seata-order-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataorderserver/SpringCloudSeataOrderServerApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudseataorderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSeataOrderServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSeataOrderServerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-seata-order-server/src/test/java/com/landasoft/demo/springcloud/springcloudseataorderserver/SpringCloudSeataOrderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudseataorderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSeataOrderServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
