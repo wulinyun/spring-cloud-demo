00ec72a5512c97424739a1f20060cd6720b4e5b3
wulinyun <lin.wu@landasoft.com>
2020-01-17 14:52:35 +0800

    账户操作mapper接口

---
+++ spring-cloud-seata-account-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/dao/AccountDao.java
@@ -0,0 +1,20 @@
+package com.landasoft.demo.springcloud.springcloudseataaccountserver.dao;
+
+import org.apache.ibatis.annotations.Param;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description  账户操作mapper接口
+ * @Date 2020/1/17 14:51
+ */
+public interface AccountDao {
+    /**
+     * 扣减账户余额
+     * @param userId 用户id
+     * @param money 金额
+     */
+    void decrease(@Param("userId") Long userId, @Param("money") BigDecimal money);
+}
