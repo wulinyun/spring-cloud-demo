a77936533b4d69c56e2b4b66dfefd81979f75522
wulinyun <lin.wu@landasoft.com>
2020-01-17 16:37:49 +0800

    库存服务

---
+++ spring-cloud-seata-storage-server/src/main/java/com/landasoft/demo/springcloud/springcloudseatastorageserver/SpringCloudSeataStorageServerApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudseatastorageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSeataStorageServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSeataStorageServerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-seata-storage-server/src/test/java/com/landasoft/demo/springcloud/springcloudseatastorageserver/SpringCloudSeataStorageServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudseatastorageserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudSeataStorageServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
