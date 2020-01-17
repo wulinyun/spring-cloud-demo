feea35d66578c06301597fd4091339a0e8912fe4
wulinyun <lin.wu@landasoft.com>
2020-01-17 15:47:48 +0800

    开启注册中心服务发现、feignClient调用

--- spring-cloud-seata-order-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataorderserver/SpringCloudSeataOrderServerApplication.java
+++ spring-cloud-seata-order-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataorderserver/SpringCloudSeataOrderServerApplication.java
@@ -1,9 +1,16 @@
 package com.landasoft.demo.springcloud.springcloudseataorderserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.landasoft.demo.springcloud.springcloudseataorderserver.dao")
+@EnableDiscoveryClient
+@EnableFeignClients
 public class SpringCloudSeataOrderServerApplication {
 
     public static void main(String[] args) {
