cd02ae25ae3d3a99f3766ac1ac1bd6cd745b034b
wulinyun <lin.wu@landasoft.com>
2020-01-17 15:11:31 +0800

    增加mapper注解

--- spring-cloud-seata-account-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/dao/AccountDao.java
+++ spring-cloud-seata-account-server/src/main/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/dao/AccountDao.java
@@ -1,5 +1,6 @@
 package com.landasoft.demo.springcloud.springcloudseataaccountserver.dao;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.math.BigDecimal;
@@ -10,6 +11,7 @@ import java.math.BigDecimal;
  * @Description  账户操作mapper接口
  * @Date 2020/1/17 14:51
  */
+@Mapper
 public interface AccountDao {
     /**
      * 扣减账户余额
