7348d9ffef654a74a3c26000d2cefe42ea844a92
wulinyun <lin.wu@landasoft.com>
2020-01-17 16:51:03 +0800

    开启服务发现、Mapper扫描

--- spring-cloud-seata-storage-server/src/main/java/com/landasoft/demo/springcloud/springcloudseatastorageserver/SpringCloudSeataStorageServerApplication.java
+++ spring-cloud-seata-storage-server/src/main/java/com/landasoft/demo/springcloud/springcloudseatastorageserver/SpringCloudSeataStorageServerApplication.java
@@ -1,9 +1,14 @@
 package com.landasoft.demo.springcloud.springcloudseatastorageserver;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@EnableDiscoveryClient
+@MapperScan("com.landasoft.demo.springcloud.springcloudseatastorageserver.dao")
 public class SpringCloudSeataStorageServerApplication {
 
     public static void main(String[] args) {
