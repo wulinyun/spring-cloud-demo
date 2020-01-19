515dfb7de0ce414edbf5d40fde441b7dc93d4eea
wulinyun <lin.wu@landasoft.com>
2020-01-19 22:15:17 +0800

    junit低版本引用低版本的依赖

--- spring-cloud-seata-storage-server/src/test/java/com/landasoft/demo/springcloud/springcloudseatastorageserver/SpringCloudSeataStorageServerApplicationTests.java
+++ spring-cloud-seata-storage-server/src/test/java/com/landasoft/demo/springcloud/springcloudseatastorageserver/SpringCloudSeataStorageServerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.landasoft.demo.springcloud.springcloudseatastorageserver;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
