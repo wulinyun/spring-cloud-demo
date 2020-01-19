dd9eb8b03e0377025578007abb813229df6f7871
wulinyun <lin.wu@landasoft.com>
2020-01-19 22:16:10 +0800

    junit低版本引用低版本的依赖

--- spring-cloud-seata-account-server/src/test/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/SpringCloudSeataAccountServerApplicationTests.java
+++ spring-cloud-seata-account-server/src/test/java/com/landasoft/demo/springcloud/springcloudseataaccountserver/SpringCloudSeataAccountServerApplicationTests.java
@@ -1,6 +1,5 @@
 package com.landasoft.demo.springcloud.springcloudseataaccountserver;
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
