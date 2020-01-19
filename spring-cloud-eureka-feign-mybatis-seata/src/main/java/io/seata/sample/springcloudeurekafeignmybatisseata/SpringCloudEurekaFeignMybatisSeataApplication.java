f8f26f7d92bf920fac99ae61e820bcac3034f539
wulinyun <lin.wu@landasoft.com>
2020-01-19 16:58:40 +0800

    spring-cloud-eureka-feign-mybatis-seata分布式事务基础pom.xml依赖

---
+++ spring-cloud-eureka-feign-mybatis-seata/src/main/java/io/seata/sample/springcloudeurekafeignmybatisseata/SpringCloudEurekaFeignMybatisSeataApplication.java
@@ -0,0 +1,13 @@
+package io.seata.sample.springcloudeurekafeignmybatisseata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaFeignMybatisSeataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaFeignMybatisSeataApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-eureka-feign-mybatis-seata/src/test/java/io/seata/sample/springcloudeurekafeignmybatisseata/SpringCloudEurekaFeignMybatisSeataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seata.sample.springcloudeurekafeignmybatisseata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaFeignMybatisSeataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
