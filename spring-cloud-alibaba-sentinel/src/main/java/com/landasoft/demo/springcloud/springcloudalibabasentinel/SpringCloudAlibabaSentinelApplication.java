a093ce8a7682601c7afd0321088f8e5333db9c84
wulinyun <lin.wu@landasoft.com>
2020-01-20 13:45:20 +0800

    spring-cloud-alibaba-sentinel项目初始化代码

---
+++ spring-cloud-alibaba-sentinel/src/main/java/com/landasoft/demo/springcloud/springcloudalibabasentinel/SpringCloudAlibabaSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudalibabasentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudAlibabaSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudAlibabaSentinelApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-alibaba-sentinel/src/test/java/com/landasoft/demo/springcloud/springcloudalibabasentinel/SpringCloudAlibabaSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.demo.springcloud.springcloudalibabasentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudAlibabaSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
