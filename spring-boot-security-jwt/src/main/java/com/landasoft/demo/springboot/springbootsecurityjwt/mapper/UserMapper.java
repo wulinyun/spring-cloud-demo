5eb962deb585726ee0cd5b112985951c1a35f701
wulinyun <lin.wu@landasoft.com>
2020-01-21 15:41:27 +0800

    用户mapper接口

---
+++ spring-boot-security-jwt/src/main/java/com/landasoft/demo/springboot/springbootsecurityjwt/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.landasoft.demo.springboot.springbootsecurityjwt.mapper;
+
+import com.landasoft.demo.springboot.springbootsecurityjwt.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description
+ * @Date 2020/1/21 15:34
+ */
+@Mapper
+public interface UserMapper {
+    @Select( "select id , username , password from user where username = #{username}" )
+    User loadUserByUsername(@Param("username") String username);
+}
