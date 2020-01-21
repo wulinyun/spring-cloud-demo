e2093cc9602bd17cbb035490cd149a9359cb0871
wulinyun <lin.wu@landasoft.com>
2020-01-21 15:32:57 +0800

    角色权限

---
+++ spring-boot-security-jwt/src/main/java/com/landasoft/demo/springboot/springbootsecurityjwt/entity/RolePermisson.java
@@ -0,0 +1,28 @@
+package com.landasoft.demo.springboot.springbootsecurityjwt.entity;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @Description
+ * @Date 2020/1/21 15:32
+ */
+public class RolePermisson {
+    private String url;
+    private String roleName;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
