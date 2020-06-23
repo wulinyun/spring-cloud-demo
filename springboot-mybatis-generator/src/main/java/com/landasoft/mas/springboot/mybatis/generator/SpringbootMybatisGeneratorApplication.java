9c85aa75fd1385e375efca138927473f7ff14ee5
wulinyun <lin.wu@landasoft.com>
2020-06-23 22:57:18 +0800

    springboot-mybatis-generator项目初始化

---
+++ springboot-mybatis-generator/src/main/java/com/landasoft/mas/springboot/mybatis/generator/SpringbootMybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.landasoft.mas.springboot.mybatis.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMybatisGeneratorApplication.class, args);
+    }
+
+}
---
+++ springboot-mybatis-generator/src/test/java/com/landasoft/mas/springboot/mybatis/generator/SpringbootMybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.landasoft.mas.springboot.mybatis.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
