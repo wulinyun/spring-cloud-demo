03811e40ec3eaa9be32aec0d9370a38f50516941
wulinyun <lin.wu@landasoft.com>
2020-06-24 00:15:24 +0800

    增加mapperScan扫描mapper，要不然就增加@Mapper注解

--- springboot-mybatis-generator/src/main/java/com/landasoft/mas/springboot/mybatis/generator/SpringbootMybatisGeneratorApplication.java
+++ springboot-mybatis-generator/src/main/java/com/landasoft/mas/springboot/mybatis/generator/SpringbootMybatisGeneratorApplication.java
@@ -1,9 +1,11 @@
 package com.landasoft.mas.springboot.mybatis.generator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.landasoft.mas.springboot.mybatis.generator.dao")
 public class SpringbootMybatisGeneratorApplication {
 
     public static void main(String[] args) {
