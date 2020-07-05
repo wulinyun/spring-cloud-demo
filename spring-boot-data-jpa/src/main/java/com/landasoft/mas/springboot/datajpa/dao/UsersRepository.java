b79a1dfbe7d3b6a1f44694ea65b03dfd706b7d55
wulinyun <lin.wu@landasoft.com>
2020-07-05 19:49:51 +0800

    jpa可以不需要注解@Repository

--- spring-boot-data-jpa/src/main/java/com/landasoft/mas/springboot/datajpa/dao/UsersRepository.java
+++ spring-boot-data-jpa/src/main/java/com/landasoft/mas/springboot/datajpa/dao/UsersRepository.java
@@ -16,6 +16,5 @@ import org.springframework.stereotype.Repository;
  * 参数一 T :当前需要映射的实体
  * 参数二 ID :当前映射的实体中的OID的类型
  */
-@Repository
 public interface UsersRepository extends JpaRepository<Users,Integer> {
 }
