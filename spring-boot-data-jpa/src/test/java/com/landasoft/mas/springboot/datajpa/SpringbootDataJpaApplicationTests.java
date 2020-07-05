aa2b4df2f12b390afb9119d7a71e1f98f466785e
wulinyun <lin.wu@landasoft.com>
2020-07-05 20:16:10 +0800

    Repository接口方法名称命名查询

--- spring-boot-data-jpa/src/test/java/com/landasoft/mas/springboot/datajpa/SpringbootDataJpaApplicationTests.java
+++ spring-boot-data-jpa/src/test/java/com/landasoft/mas/springboot/datajpa/SpringbootDataJpaApplicationTests.java
@@ -1,16 +1,21 @@
 package com.landasoft.mas.springboot.datajpa;
 
 import com.landasoft.mas.springboot.datajpa.dao.UsersRepository;
+import com.landasoft.mas.springboot.datajpa.dao.UsersRepositoryByName;
 import com.landasoft.mas.springboot.datajpa.entity.Roles;
 import com.landasoft.mas.springboot.datajpa.entity.Users;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class SpringbootDataJpaApplicationTests {
     @Autowired
     private UsersRepository usersRepository;
+    @Autowired
+    private UsersRepositoryByName usersRepositoryByName;
     @Test
     void contextLoads() {
     }
@@ -36,5 +41,27 @@ class SpringbootDataJpaApplicationTests {
         users.setName("王五");
         //this.usersRepository.deleteById(1);
         this.usersRepository.delete(users);
+    }
+    @Test
+    public void usersRepositoryByName(){
+        List<Users> list = this.usersRepositoryByName.findByName("王五");
+        for(Users users : list){
+            System.out.println(users.toString());
+        }
+
+    }
+    @Test
+    public void usersRepositoryByNameAndAge(){
+        List<Users> usersList = this.usersRepositoryByName.findByNameAndAge("王五",29);
+        for(Users users:usersList){
+            System.out.println(users.toString());
+        }
+    }
+    @Test
+    public void findByNameLike(){
+        List<Users> usersList = this.usersRepositoryByName.findByNameLike("王%");
+        for(Users users:usersList){
+            System.out.println(users.toString());
+        }
     }
 }
