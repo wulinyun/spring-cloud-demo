07eff00d2420cf1d861d7a775e457d71a5676b2e
wulinyun <lin.wu@landasoft.com>
2021-03-11 17:17:51 +0800

    两个根子组成最大的正方形的算法

---
+++ spring-boot-jar/src/main/java/com/landasoft/demo/springboot/springbootjar/Solution.java
@@ -0,0 +1,49 @@
+package com.landasoft.demo.springboot.springbootjar;
+
+/**
+ * @Author wulinyun
+ * @Version 1.0
+ * @JdkVesion 1.7
+ * @Description TODO
+ * @Date 2021/3/11 11:03
+ */
+public class Solution {
+    public static int solution(int A, int B){
+        int avg = (A+B)/4;
+        if(avg <= 0 ){
+            return 0;
+        }
+        int a_length = A/avg;
+        int b_length = B/avg;
+        int length = a_length + b_length;
+        if(length == 4){
+            return avg;
+        }else{
+            while(avg>0 && length != 4){
+                avg = avg - 1;
+                if(avg > 0) {
+                    length = length(A, B, avg);
+                }
+            }
+            if(length == 4){
+                return avg;
+            }
+
+        }
+        return 0;
+    }
+
+    public static int length(int A,int B,int avg){
+        if(avg <= 0 ){
+            return 0;
+        }
+        int a_length = A/avg;
+        int b_length = B/avg;
+        int length = a_length + b_length;
+        return length;
+    };
+
+    public static void main(String[] args) {
+        System.out.println(solution(1,8));
+    }
+}
